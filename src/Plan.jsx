b3de3c8b52a43e180a8cf4787ac627f7966cb7b4
Cdemz <gbengaadepoju75@gmail.com>
2023-05-04 22:50:17 +0100

    create

---
+++ src/Plan.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Plan = () => {
+  return (
+    <div className="text-orange-500">
+      {' '}
+      <p>Plan</p>{' '}
+    </div>
+  );
+};
+
+export default Plan;
